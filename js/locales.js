7a7c5aa4a76a8d01c9393187f7027ed0ae22803c
chucho0rtiz <jesusstivenortiz@gmail.com>
2020-10-27 23:19:53 -0500

    first commit

---
+++ js/locales.js
@@ -0,0 +1,403 @@
+var locales = [{
+        "nombre": "Jhon perrada",
+        "facebook": "https://www.facebook.com/JhonPerrada",
+        "instagram": "http://instagram.com/jhonperrada/",
+        "descripcion": "Estadero de cocina callejera y de autor. 14 años de historia en el Corregimiento de San Cristóbal. Aquí Vale la pena esperar y compartir en Familia. Bienvenidos. ",
+        "horario": "Horario de atención: lunes, miércoles y viernes: cerrado  Jueves, viernes, sábado y domingo: de 17:00 a 22:00 ",
+        "telefono": "302 2296614",
+        "direccion": "Calle 63 #126-53 050036",
+        "imagenes": "images/logos_Restaurantes/jhonPerrada.jpeg",
+        "modal":"Jhon_perrada",
+        "modalCarousel":"Jhon_perrada_Carousel",
+        "Menu": [
+            "images/menu/Jhon_Perrada/Jhon_Perrada_2.jpeg",
+            "images/menu/Jhon_Perrada/Jhon_Perrada_3.jpeg",
+            "images/menu/Jhon_Perrada/Jhon_Perrada_4.jpeg",
+            "images/menu/Jhon_Perrada/Jhon_Perrada_5.jpeg",
+            "images/menu/Jhon_Perrada/Jhon_Perrada_6.jpeg",
+            "images/menu/Jhon_Perrada/Jhon_Perrada_Inf.jpeg",
+        ]
+    },
+    {
+        "nombre": "Frulatto",
+        "facebook": "#",
+        "instagram": "https://www.instagram.com/frulatto_oficial/?hl=es-la",
+        "descripcion": "Deliciosos sabores de helado, con su salsa especial, que te dará una explosión de sabores en cada bocado. ",
+        "horario": "Horario de atención: todos los días de 12:00 a 20:00",
+        "telefono": "304 4626112",
+        "direccion": "",
+        "imagenes": "images/logos_Restaurantes/frulatto.jpg",
+        "modal":"Frulatto",
+        "modalCarousel":"Frulatto_Carousel",
+        "Menu": [
+            "images/menu/Frulatto/Frulatto_2.jpeg",
+            "images/menu/Frulatto/Frulatto_Inf.jpeg",
+        ]
+    },
+    {
+        "nombre": "En la salida picnic",
+        "facebook": "https://www.facebook.com/enlasalidapicnic",
+        "instagram": "#",
+        "descripcion": "Ambiente campestre para disfrutar en familia una tarde o noche súper agradable con amplia gastronomía y excelente servicio.",
+        "horario": "Horario de atención: Lunes: lunes, martes, miércoles y viernes: cerrado Viernes: 17:00 - 12:00, Sábado y domingo: 0:00 – 12:00",
+        "telefono": "3046545849",
+        "direccion": "",
+        "imagenes": "images/logos_Restaurantes/enLaSalida.jpg",
+        "modal":"En_la_salida_picnic",
+        "modalCarousel":"En_la_salida_picnic_Carousel",
+        "Menu": [
+            "images/menu/EnLaSalidaPicnic/EnLaSalidaPicnic_2.jpeg",
+            "images/menu/EnLaSalidaPicnic/EnLaSalidaPicnic_3.jpeg",
+            "images/menu/Frulatto/EnLaSalidaPicnic_Inf.jpeg",
+        ]
+    }, {
+        "nombre": "La baguette",
+        "facebook": "#",
+        "instagram": "https://www.instagram.com/labaguette3/?hl=es-la",
+        "descripcion": "Nuestros productos son hechos con las manos de la abuela, materia prima consciente y mucho amor",
+        "telefono": "319 305 27 39",
+        "horario": "",
+        "direccion": "",
+        "imagenes": "images/logos_Restaurantes/laBaguette.jpg",
+        "modal":"La_baguette",
+        "modalCarousel":"La_baguette_Carousel",
+        "Menu": [
+            "images/menu/EnLaSalidaPicnic/EnLaSalidaPicnic_2.jpeg",
+        ]        
+    }, {
+        "nombre": "Pizzachos",
+        "facebook": "https://www.facebook.com/pizzachosyshawarma",
+        "instagram": "#",
+        "descripcion": "Variedad de deliciosas pizzas y comida mexicana sin igual que podrás disfrutar en cada bocado",
+        "horario": "Horario de atención: lunes y martes: cerrado Miércoles, jueves: 17:00 -22:00 Viernes, sábado: 17:00 - 23:00 Domingo: 17:00 – 23:30",
+        "telefono": "311 6608308",
+        "direccion": "",
+        "imagenes": "images/logos_Restaurantes/pizzachos.jpeg",
+        "modal":"Pizzachos",
+        "modalCarousel":"Pizzachos_Carousel",
+        "Menu": [
+            "images/menu/Pizzachos/Pizzachos2.jpeg",
+            "images/menu/Pizzachos/Pizzachos3.jpeg",
+            "images/menu/Pizzachos/Pizzachos4.jpeg",
+            "images/menu/Pizzachos/Pizzachos5.jpeg",
+            "images/menu/Pizzachos/Pizzachos6.jpeg",
+            "images/menu/Pizzachos/Pizzachos7.jpeg",
+            "images/menu/Pizzachos/PizzachosInfo.jpeg",
+        ]
+    }, {
+        "nombre": "Café express",
+        "facebook": "https://www.facebook.com/cafexpresspapas",
+        "instagram": "https://instagram.com/cafexpresspapas?igshid=txpetnnbtptl",
+        "descripcion": "¡Bienvenido a Café Express! Somos un lugar donde encuentras un amplio Menú de comidas rápidas y helados, en el que puedes disfrutar en un espacio agradable con familia y amigos.🤤😋 Encuéntranos: Al frente de la plazoleta de San Cristóbal.",
+        "horario": "Horario de atención: todos los días: 13:30 a 19:30",
+        "telefono": "304 6788341 – 304 4743692",
+        "direccion": "",
+        "imagenes": "images/logos_Restaurantes/cafeExpress.jpeg",
+        "modal":"Cafe_express",
+        "modalCarousel":"Cafe_express_Carousel",
+        "Menu": [
+            "images/menu/CafeExpress/CafeExpress2.jpeg",
+            "images/menu/CafeExpress/CafeExpress3.jpeg",
+            "images/menu/CafeExpress/CafeExpress4.jpeg",
+            "images/menu/CafeExpress/CafeExpress5.jpeg",
+            "images/menu/CafeExpress/CafeExpress6.jpeg",
+            "images/menu/CafeExpress/CafeExpress7.jpeg",
+            "images/menu/CafeExpress/CafeExpress8.jpeg",
+            "images/menu/CafeExpress/CafeExpressInfp.jpeg",
+        ]        
+    },
+    {
+        "nombre": "Jugos y Q´banos del loro",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "Sanduches que podrás disfrutar acompañado de un buen jugo natural que te harán desear repetir su distintivo sabor",
+        "horario": "Horario de atención: todos los días 7:00 – 22:00",
+        "telefono": "312 8429422 por vinser",
+        "direccion": "",
+        "imagenes": "images/logos_Restaurantes/qBanos.jpg",
+        "modal":"Qbanos",
+        "modalCarousel":"Qbanos_Carousel",
+        "Menu": [
+            "images/menu/qBanos/qBanos1.jpeg",
+        ]        
+    },
+    {
+        "nombre": "Punto y coma",
+        "facebook": "https://www.facebook.com/Punto-y-coma-de-todo-un-poco-606016953175780",
+        "instagram": "#",
+        "descripcion": "Tú comida rápida a la puerta de tú casa en tan solo unos minutos de responderte, no te lo pierdas y has tú pedido para que disfrutes más de un poco de todo",
+        "horario": "Horario de atención: todos los días de 12:00 -22:00",
+        "telefono": "3012243134",
+        "direccion": "",
+        "imagenes": "images/logos_Restaurantes/punto&Coma.jpeg",
+        "modal":"Punto_coma",
+        "modalCarousel":"Punto_coma_Carousel",
+        "Menu": [
+            "images/menu/PuntoComa/PuntoComa2.jpeg",
+            "images/menu/PuntoComa/PuntoComaInfo.jpeg",
+        ]        
+    },
+    {
+        "nombre": "Carnelly",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "Disfruta de tu comida rápida al estilo Xiaomi lo mejor en calidad precio ",
+        "horario": "Horario de atención: todos los días 12:00 – 20:00",
+        "telefono": "312 8429422 por vinser",
+        "direccion": "Calle 63 crra 131 #62 16 ",
+        "imagenes": "images/logos_Restaurantes/carnelly.jpg",
+        "modal":"Carnelly",
+        "modalCarousel":"Carnelly_Carousel",
+        "Menu": [
+            "images/menu/Carnelly/Carnelly2.jpeg",
+            "images/menu/Carnelly/CarnellyInf.jpeg",
+        ]        
+    },
+    {
+        "nombre": "The ghost",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "Prueba nuestros granizados con o sin licor y pide al por mayor ",
+        "horario": "Horario de atención: lunes, martes y miércoles: cerrado, Jueves: 19:00 -2:00, Viernes, sábados y domingos: 16:30 – hasta acabar producción",
+        "telefono": "322 5872124",
+        "direccion": " ",
+        "imagenes": "images/logos_Restaurantes/granizados.jpg",
+        "modal":"The_ghost",
+        "modalCarousel":"The_ghost_Carousel",
+        "Menu": [
+            "images/menu/Theghost/Theghost1.jpeg",
+        ]        
+    },
+    {
+        "nombre": "Habibi shawarma",
+        "facebook": "https://www.facebook.com/habibishawarma2019",
+        "instagram": "https://www.instagram.com/habibishawarma2019/?hl=es-la",
+        "descripcion": "Prueba nuestras finas rebanadas de carne y nuestra deliciosa sazón traído del Medio Oriente",
+        "horario": "Horario de atención: lunes y martes: cerrado  Miércoles, jueves: 17:00 -22:00 Viernes, sábado: 17:00 - 23:00 Domingo: 17:00 – 23:30",
+        "telefono": "311 6608308",
+        "direccion": "Calle 62 # 127-96 San Cristóbal, Colombia",
+        "imagenes": "images/logos_Restaurantes/habibi.jpeg",
+        "modal":"Habibi",
+        "modalCarousel":"Habibi_Carousel",
+        "Menu": [
+            "images/menu/Habibi/Habibi1.jpeg",
+            "images/menu/Habibi/Habibi2.jpeg",
+        ]        
+    }, {
+        "nombre": "Mohos burguer",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "Disfruta nuestras deliciosas hamburguesas artesanales",
+        "horario": "",
+        "telefono": "316 5161298 – 3014119445",
+        "direccion": " ",
+        "imagenes": "images/logos_Restaurantes/mohos.jpg",
+        "modal":"Mohos_burguer",
+        "modalCarousel":"Mohos_burguer_Carousel",
+        "Menu": [
+            "images/menu/Mohos/Mohos1.jpeg",
+        ]        
+    }, {
+        "nombre": "Tiernitas",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "Prueba la gran variedad de arepas y patacones que tenemos para ti ¡no te quedes con las ganas!",
+        "horario": "Horario de atención: todos los días: 12:00 – 22:00",
+        "telefono": "312 8429422 por vinser",
+        "direccion": "Calle 63 crra 131 #62 16",
+        "imagenes": "images/logo.jpg",
+        "modal":"Tiernitas",
+        "modalCarousel":"Tiernitas_Carousel",
+        "Menu": [
+            "images/menu/Tiernitas/Tiernitas2.jpeg",
+            "images/menu/Tiernitas/Tiernitas3.jpeg",
+            "images/menu/Tiernitas/Tiernitas4.jpeg",
+            "images/menu/Tiernitas/TiernitasInf.jpeg",
+        ]        
+    }, {
+        "nombre": "Wings and beer y los que dicen de casa",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "",
+        "horario": "",
+        "telefono": "",
+        "direccion": " ",
+        "imagenes": "images/logos_Restaurantes/wingsAndBeer.jpg",
+        "modal":"Wings",
+        "modalCarousel":"Wings_Carousel",
+        "Menu": [
+            "images/menu/Wings/WingsInfo.jpeg",
+        ]        
+    },
+];
+
+
+locales.forEach(e => {
+    $j('#slide-empresas').append(`
+    <li>
+    <div class="team-post">
+        <div class="container">
+            <div class="item-content">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="title">
+                            <span>San Comidas</span>
+                            <h1>${e.nombre}</h1>
+                        </div>
+                        <p>${e.descripcion}</p>
+                        <div class="social-share">
+                            <ul class="social-icons">
+                                <li><a target="_blank" href="${e.facebook}">Facebook</a></li>
+                                <li><a target="_blank" href="${e.instagram}">Instagram</a></li>
+                            </ul>
+                        </div>
+                        <div class="pagination-btn">
+                            <a href="#"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="image-holder">
+            <img src="${e.imagenes==""?'upload/others/a4.jpg':e.imagenes}" alt="">
+    </div>
+        </div>
+</li>
+    `);
+
+    $j('#ubicacion-locales').append(`
+            <div class="col-md-6">
+            <div class="card mb-3 shadow" style="max-width: 540px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4 p-2 mt-5">
+                        <img src="${e.imagenes==""?'upload/others/a4.jpg':e.imagenes}" class="card-img" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${e.nombre}</h5>
+                            <p class="card-text">${e.descripcion}</p>
+                           <div class="p-1 rounded" style="background-color: gray; color: white !important;">
+                                <h2 style="color: white;">${e.direccion}</h2>
+                                <h2 style="color: white;">${e.telefono}</h2>    
+                           </div>          
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+
+    $j('#menu-locales').append(`
+        <div class="col-4 mt-2">
+        <div class="card shadow p-2" style="width: 18rem; align-items: center;">
+            <img src="${e.imagenes==""?'upload/others/a4.jpg':e.imagenes}" style="width: 150px; height: 150px;" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${e.nombre}</h5>
+                <p class="card-text">${e.direccion} ${e.telefono}</p>
+                <button type="button" class="btn btn-dark shadow" data-toggle="modal" data-target="${'#'+e.modal}">
+                    Ver menu de <span style="color: #FF9900">${e.nombre}</span>
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="${e.modal}" tabindex="-1" aria-labelledby="${e.modal+"Label"}" aria-hidden="true">
+                    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="${e.modal+"Label"}">Menú</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <!-- MENU -->
+                            <div>
+                                <style>
+                                    .carousel {
+                                        max-width: 800px;
+                                        margin: 0 auto;
+                                        display: flex;
+                                    }
+                                    #imagen {
+                                        width: 100%;
+                                        height: 400px;
+                                        background-size: cover;
+                                    }
+                                    #avanzar, #retroceder{
+                                        margin-top: 50%;
+                                        border-radius: 25px 25px;
+                                        color: #ffffff;
+                                        background-color: #ff9900;
+                                        height: 20%;
+                                        padding: 9px;
+                                    }
+                                </style>
+                                <div class="carousel">
+                                    <button id="retroceder">Retroceder</button>
+                                    <div id=${e.modalCarousel}></div>
+                                    <button id="avanzar">Avanzar</button>
+                                </div>
+                            </div>
+                            <!-- FIN MENU -->
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `);
+});
+
+window.onload = function () {
+    // Variables
+    
+    let posicionActual = 0;
+    let $botonRetroceder = document.querySelector('#retroceder');
+    let $botonAvanzar = document.querySelector('#avanzar');
+    locales.forEach(e => {
+        let $imagen = document.querySelector('#'+e.modalCarousel);
+    
+        // Funciones
+    
+        /**
+         * Funcion que cambia la foto en la siguiente posicion
+         */
+        function pasarFoto() {
+            if(posicionActual >= e.Menu.length - 1) {
+                posicionActual = 0;
+            } else {
+                posicionActual++;
+            }
+            renderizarImagen();
+        }
+    
+        /**
+         * Funcion que cambia la foto en la anterior posicion
+         */
+        function retrocederFoto() {
+            if(posicionActual <= 0) {
+                posicionActual = e.Menu.length - 1;
+            } else {
+                posicionActual--;
+            }
+            renderizarImagen();
+        }
+    
+        /**
+         * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
+         */
+        function renderizarImagen () {
+            $imagen.style.backgroundImage = `url(${e.Menu[posicionActual]})`;
+        }
+    
+        // Eventos
+        $botonAvanzar.addEventListener('click', pasarFoto);
+        $botonRetroceder.addEventListener('click', retrocederFoto);
+        // Iniciar
+        renderizarImagen();
+    });
+}
+
